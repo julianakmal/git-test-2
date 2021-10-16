6139eb0a347fe396a1b50318d10c2c70ffc95d42
Julian Akmal <j4kmal@gmail.com>
2021-10-16 12:33:51 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coba Git Remote</title>
-    </head>
+</head>
+
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga omnis et atque corporis nemo beatae pariatur expedita quidem? Iure aliquam mollitia non deserunt eveniet at nam voluptatem voluptatum, officiis ipsa!</p>
+    <ul>
+        <li>uno</li>
+        <li>dos</li>
+    </ul>
 </body>
+
 </html>
